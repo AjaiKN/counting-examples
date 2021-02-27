8693fbef0e15c1eac0d3cf25cf6b09cd0a83b6f9
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-02-26 22:19:58 -0500

    Allow control numStars

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-	<StarsBars :numStars="15" />
+	<StarsBars :numStars="numStars" :key="numStars" />
+
+	<label>
+		Number of stars
+		<input
+			type="range"
+			v-model.number="numStars"
+			:min="minStars"
+			:max="maxStars"
+		/>
+	</label>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import StarsBars from './components/StarsBars.vue'
+
+const numStars = ref(15)
+
+const minStars = 4
+const maxStars = 15
 </script>
--- src/useTweened.ts
+++ src/useTweened.ts
@@ -4,7 +4,6 @@ import gsap from 'gsap'
 export function useTweened(original: Ref<number>, options: any = {}) {
 	const tweenedObject = reactive({ value: original.value })
 	watch(original, (newVal) => {
-		console.log(newVal)
 		gsap.to(tweenedObject, { value: newVal, ...options })
 	})
 	return computed(() => tweenedObject.value)
